8bc22ad300a8b3cfb5256fe3ac4cb07ae1669c76
Mladenka Orlić <mladenka.orlic@gmail.com>
2024-12-07 17:59:03 +0000

    passes tests 1-8, #8 solved with duration parseInt

--- index.js
+++ index.js
@@ -1,18 +1,119 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const db = require("./db");
+const app = express();
+const cors = require("cors");
+const { ObjectId } = require("mongodb");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+//TEST ROUTE
+//create a test BOOK collection inside a db, insert one document into it
+app.get("/mlaksi", async (req, res) => {
+  try {
+    const database = db.getDb();
+    await database.collection("books").insertOne({
+      title: "The Count of Monte Cristo",
+      author: "Alexandre Dumas",
+      year: 1844,
+      genres: ["Adventure", "Historical Fiction"],
+      available: true,
+    });
+    res.status(200).send("Book inserted successfully.");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error inserting book.");
+  }
+});
 
+app.post("/api/users", async (req, res) => {
+  //res.json({ test: req.body.username });
+  try {
+    const database = db.getDb();
+    await database
+      .collection("users")
+      .insertOne({ username: req.body.username });
+    const userObject = await database
+      .collection("users")
+      .findOne({ username: req.body.username });
+    //console.log(userObject);
+    //console.log(typeof userObject);
+    res.json({ username: req.body.username, _id: userObject._id });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error inserting user.");
+  }
+});
 
+app.get("/api/users", async (req, res) => {
+  try {
+    const database = db.getDb();
+    const users = await database.collection("users").find().toArray();
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error fetching users.");
+  }
+});
 
+// 7. You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
+// 8. The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  try {
+    const database = db.getDb();
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+    //pick up user data from form and route params
+    const formattedDate = new Date(req.body.date).toDateString();
+    const duration = req.body.duration;
+    const description = req.body.description;
+    const id = new ObjectId(req.params._id);
+
+    //find user with corresponding id
+    const user = await database.collection("users").findOne({ _id: id });
+
+    //if there is no user, respond with error
+    if (!user) {
+      res.status(404).send("User not found");
+    }
+
+    const stringId = user._id.toString();
+
+    await database.collection("exercises").insertOne({
+      userid: user._id,
+      username: user.username,
+      date: formattedDate,
+      duration: duration,
+      description: description,
+    });
+
+    // await database
+    //   .collection("users")
+    //   .updateOne(
+    //     { _id: user._id },
+    //     { $push: { exercises: { date: formattedDate, duration, description } } }
+    //   );
+
+    // const updatedUser = await database
+    //   .collection("users")
+    //   .findOne({ _id: user._id });
+    const exercise=await database.collection("exercises").findOne({userid:user._id});
+
+    res.json({username:exercise.username,description:exercise.description,duration:parseInt(exercise.duration),date:exercise.date,_id:exercise.userid});
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error inserting exercise.");
+  }
+});
+
+db.connectToDatabase().then(() => {
+  const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+  });
+});
